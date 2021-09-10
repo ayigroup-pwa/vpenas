e1fd38b08c159ececbc3ee88318a0fc2ba432e7b
vitipe <victorcasbas@gmail.com>
2021-09-10 13:37:02 -0300

    Adds IDB-Read files

--- public/src/js/app.js
+++
@@ -1,9 +0,0 @@
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    //.register('/sw.js', {scope: '/help/'}) HTTPS: requested for pwa sw
-    .register('/sw.js')
-    .then(function() {
-      console.log('Service worker registered!');
-    });
-}
